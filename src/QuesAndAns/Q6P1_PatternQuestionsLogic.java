53a00c5244425d0b679fde6d9ca504d721584694
Parvez Akhtar <parvezakhtar310@gmail.com>
2024-01-06 17:11:24 +0530

    intial commit

--- src/QuesAndAns/Q6P1_PatternQuestionsLogic.java
+++ src/QuesAndAns/Q6P1_PatternQuestionsLogic.java
@@ -1,6 +1,6 @@
 package QuesAndAns;
 
-public class Q6_PatternQuestionsLogic {
+public class Q6P1_PatternQuestionsLogic {
     public static void main(String[] args) {
         for(int i =0; i<=4;i++){
             for (int j=0; j<=i;j++){
