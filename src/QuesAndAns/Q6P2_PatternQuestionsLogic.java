ebf95d210bdb659647062c391dcc08909502bddb
Parvez Akhtar <parvezakhtar310@gmail.com>
2024-01-06 17:13:46 +0530

    intial commit

--- src/QuesAndAns/Q6P2_PatternQuestionsLogic.java
+++ src/QuesAndAns/Q6P2_PatternQuestionsLogic.java
@@ -11,5 +11,15 @@ public class Q6P2_PatternQuestionsLogic {
             }
             System.out.println();
         }
+        System.out.println("---------------------------------");
+        for (int i=0;i<=4;i++){
+            for (int j = 4; j>i;j--){
+                System.out.print(" ");
+            }
+            for (int k=0;k<=i;k++){
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
     }
 }
